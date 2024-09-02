7ebbda5fe70447ff17d4fd6d5cc1675f125c03e3
Chidera-Mercy <derarose2006@gmail.com>
2024-09-02 05:55:53 +0000

    initial commit

---
+++ src/app/layout/navbar/category/category.model.ts
@@ -0,0 +1,12 @@
+import { IconName } from "@fortawesome/fontawesome-svg-core"
+
+export type CategoryName = "ALL" | "AMAZING_VIEWS" | "TREEHOUSES"
+ | "BEACH" | "FARMS" | "TINY_HOMES" | "LAKE" | "CONTAINERS" | "CAMPING" | "CASTLE" | "SKIING" 
+ | "CAMPERS" | "ARTIC" | "BOAT" | "BED_AND_BREAKFASTS" | "ROOMS" | "EARTH_HOMES" | "TOWER" | "CAVES" | "LUXES" | "CHEFS_KITCHEN" | "OMG"
+
+export interface Category {
+    icon: IconName
+    displayName: string,
+    technicalName: CategoryName,
+    activated: boolean
+}
